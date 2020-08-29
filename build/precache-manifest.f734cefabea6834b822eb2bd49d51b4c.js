755de60c061205821449ce3295b11d32540ccee2
Maria Kotliarova <itwassoclear@gmail.com>
2020-08-29 20:46:52 +0300

    changes for deploy

---
+++ build/precache-manifest.f734cefabea6834b822eb2bd49d51b4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc3e7a69555795e2d7cb73206897040",
+    "url": "/demo-prj/index.html"
+  },
+  {
+    "revision": "e64ab611568e03fc3689",
+    "url": "/demo-prj/static/css/main.3d7ce3de.chunk.css"
+  },
+  {
+    "revision": "21e662e08b005a4b3d50",
+    "url": "/demo-prj/static/js/2.30334a10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-prj/static/js/2.30334a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e64ab611568e03fc3689",
+    "url": "/demo-prj/static/js/main.c08d2500.chunk.js"
+  },
+  {
+    "revision": "4f3b31df9e4f961a9078",
+    "url": "/demo-prj/static/js/runtime-main.5085d27f.js"
+  },
+  {
+    "revision": "286a88b7b79f68e676815be7ba319a5e",
+    "url": "/demo-prj/static/media/black.286a88b7.svg"
+  },
+  {
+    "revision": "a1956fe5228aadba324618188e7b36ed",
+    "url": "/demo-prj/static/media/red.a1956fe5.svg"
+  }
+]);
